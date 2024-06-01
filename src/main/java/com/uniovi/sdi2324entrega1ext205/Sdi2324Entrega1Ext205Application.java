5d5a00e72bfc2b7ce1533e132b1b7a6bcb9a1df5
pablo <uo271116@uniovi.es>
2024-06-01 18:39:41 +0200

    test commit

---
+++ src/main/java/com/uniovi/sdi2324entrega1ext205/Sdi2324Entrega1Ext205Application.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi2324entrega1ext205;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2324Entrega1Ext205Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2324Entrega1Ext205Application.class, args);
+    }
+
+}
